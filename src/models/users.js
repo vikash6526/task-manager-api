686fcc0541e8c2bdbd6fe400ff5a2a08e835f970
vikash6526 <vikashkumar1003@gmail.com>
2020-10-27 01:26:55 -0400

    user module updated

--- src/models/users.js
+++ src/models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const { resolveContent } = require('nodemailer/lib/shared')
+    //const { resolveContent } = require('nodemailer/lib/shared')
 const Task = require('./tasks')
 
 const userSchema = new mongoose.Schema({
